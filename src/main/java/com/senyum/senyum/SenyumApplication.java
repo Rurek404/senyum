9002192bdf4265f47b72e7695817844ca7256ab0
pgondek <pgondek@inpost.pl>
2023-11-24 21:45:33 +0100

    init

---
+++ src/main/java/com/senyum/senyum/SenyumApplication.java
@@ -0,0 +1,13 @@
+package com.senyum.senyum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenyumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenyumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/senyum/senyum/SenyumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senyum.senyum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenyumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
