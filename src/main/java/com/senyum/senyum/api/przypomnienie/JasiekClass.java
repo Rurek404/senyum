1bdcc91508b1b81194dd17741a3a976c7442650b
pgondek <pgondek@inpost.pl>
2023-11-26 15:59:39 +0100

    SEN-1 rozszerzenie zaleznosci w pom.xml oraz dodanie przykladowego rest controllera

---
+++ src/main/java/com/senyum/senyum/api/przypomnienie/JasiekClass.java
@@ -0,0 +1,12 @@
+package com.senyum.senyum.api.przypomnienie;
+
+public class JasiekClass {
+
+    public String greetings() {
+        return "adfafsa";
+    }
+
+    public int addition(int a, int b) {
+        return a+b;
+    }
+}
