609a20d4f0a812e795717f030dc5c61790cf3112
Rurek404 <jasiekslosarczyk2@gmail.com>
2023-11-26 17:14:00 +0100

    added 3 methods to JasiekClass

--- src/main/java/com/senyum/senyum/api/przypomnienie/JasiekClass.java
+++ src/main/java/com/senyum/senyum/api/przypomnienie/JasiekClass.java
@@ -9,4 +9,34 @@ public class JasiekClass {
     public int addition(int a, int b) {
         return a+b;
     }
+
+    public void showAllNumbersInRange(int a, int b) {
+        while(a <= b) {
+            System.out.println(a);
+            a++;
+        }
+    }
+
+    public int sumFrom0(int a) {
+        int sum = 0;
+        for(int i = 0; i <= a; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public void toBin(int a) {
+        int rests[] = new int[30];
+        int i = 0;
+        while (a > 0) {
+            rests[i] = a % 2;
+            a = a / 2;
+            i++;
+        }
+        for (int j = i - 1; j >= 0; j--) {
+            System.out.print(rests[j]);
+        }
+
+    }
+
 }
